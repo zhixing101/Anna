a6de7e4fd9a4815f90774d80d59b26171c295567
jiangre <10189838@qq.com>
2023-05-21 08:08:39 +0900

    new mycalendar

---
+++ Source/mycalendar/pix/function name_of_function(){.js
@@ -0,0 +1,12 @@
+function name_of_function(){
+}
+var penColour = 'black';
+
+function setPenColour(pen)
+{
+    penColour = pen;
+}
+function setPixelColour(pixel)
+{
+    pixel.style.background = penColour;
+}
